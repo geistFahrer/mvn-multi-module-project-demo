92e0c13de0332b6e6c7e2af87bedb5aad57ff4ab
Bishu Roy <biroy1393@gmail.com>
2022-05-01 11:44:28 +0530

    Added jpa-entities module

---
+++ jpa-entities/src/main/java/geist/fahrer/entity/User.java
@@ -0,0 +1,23 @@
+package geist.fahrer.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class User {
+	
+	@Id
+	private int id;
+	private String firstName;
+	private String lastName;
+	private String email;
+
+}
